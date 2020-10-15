fb115f2b60f22a76df0b4e800d5eb8ad184d38ca
sumanbattu <battu.suman94@gmail.com>
2020-10-15 18:43:22 +0530

    moditying data

--- suman.java
+++ suman.java
@@ -2,3 +2,6 @@ Class {
 	public static void main(Strings args[])
 	{System.out.pritln("My nane sis suman :")
 	}
+
+	Adding Content here... i can edit a file which resides in staging area also... i
+		I can edit a file which is in Local Repo also..
